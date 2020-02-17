7a62cd96181cca788f00be0a529d1c57550ed7d4
MarshallAlvarez <58570108+MarshallAlvarez@users.noreply.github.com>
2020-02-17 19:43:36 -0300

    Add files via upload

---
+++ Curso de Python CFB Cursos/Aula19.py.py
@@ -0,0 +1,21 @@
+
+n1 = 10 ; n2 = 5
+
+def propaganda_CFB_Cursos( ) :
+    
+    print ( "\nCurso de Python - CFB Cursos" )
+    
+    print ( "\nyoutube.com/cfbcursos\n" )
+    
+n1 = 10 ; n2 = 5
+
+def soma( ) : r = n1 + n2 ; print ( '\n' + str ( n1 ) + " + " + str ( n2 ) + " = " + str ( r ) )
+def subt( ) : r = n1 - n2 ; print ( '\n' + str ( n1 ) + " - " + str ( n2 ) + " = " + str ( r ) )
+def mult( ) : r = n1 * n2 ; print ( '\n' + str ( n1 ) + " * " + str ( n2 ) + " = " + str ( r ) )
+def divi( ) : r = n1 / n2 ; print ( '\n' + str ( n1 ) + " / " + str ( n2 ) + " = " + str ( r ) )
+    
+def calculos( ) : soma( ) ; subt( ) ; mult( ) ; divi( )
+
+propaganda_CFB_Cursos( )
+
+calculos( )
