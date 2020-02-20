ddf0d37a24a210646f0424ac09328a2f7ec0a135
MarshallAlvarez <58570108+MarshallAlvarez@users.noreply.github.com>
2020-02-20 20:42:01 -0300

    Add files via upload

---
+++ Curso de Python CFB Cursos/Aula21.py
@@ -0,0 +1,18 @@
+
+valores = [ 1 , 5 , 3 , 2 , 10 , 4 , 8 ]
+
+def somar( num ) :
+
+    r = 0 
+
+    for n in num :
+
+        r += n
+
+    return r
+
+r = somar( valores )
+
+print ( '\n' + str( valores ) + " = " + str( r ) )
+
+
