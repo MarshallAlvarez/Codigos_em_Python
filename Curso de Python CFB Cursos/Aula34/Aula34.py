750e24b6179a56d37c7f966a085a6bf2a53bfcbd
MarshallAlvarez <58570108+MarshallAlvarez@users.noreply.github.com>
2020-03-31 20:23:25 -0300

    Add files via upload

---
+++ Curso de Python CFB Cursos/Aula34/Aula34.py
@@ -0,0 +1,27 @@
+
+""" 
+    import canal as cn 
+
+    #from canal import jogador 
+    
+    cn.canal_nome()
+
+    print( cn.jogador[ "Nome" ] )
+
+    print( "\n\n" + jogador[ "Nome" ] + "\n\n" )
+"""
+
+import canal
+
+res = dir( canal )
+
+print( res )
+
+
+
+
+
+
+
+
+
---
+++ Curso de Python CFB Cursos/Aula34/canal.py
@@ -0,0 +1,10 @@
+
+def canal_nome() :
+    
+    print( "\n\nCFBC Cursos\n\n" )
+
+jogador = {
+            "Nome"    : "Bruce" ,
+            "Time"    : 1       ,
+            "Pontos"  : 100     ,
+            "Energia" : 75      }
