7e45e202c78d4cf6933b3466a7814c3315b98322
MarshallAlvarez <58570108+MarshallAlvarez@users.noreply.github.com>
2020-04-03 15:32:04 -0300

    Add files via upload

---
+++ Curso de Python CFB Cursos/Aula36.py
@@ -0,0 +1,48 @@
+
+import json
+
+"""
+    carros_json = ' { "Marca" : "Honda" , "Modelo" : "HRV" , "Cor" : "Prata" } '
+
+    carros = json.loads( carros_json )
+
+    print( "\n\n" + str( carros ) + '\n' )
+
+    print( carros[ "Marca"  ] )
+    print( carros[ "Modelo" ] )
+    print( carros[ "Cor"    ] ) 
+
+
+    print( '\n' ) 
+    for i in carros          : print( i )
+
+    print( '\n' ) 
+    for i in carros.values() : print( i )
+
+    print( '\n' ) 
+    for i in carros.items () : print( i )
+
+    for i , j in carros.items() : 
+        
+        print( i + ' - ' + j )
+"""
+carros = {
+
+    "Marca"  : "Honda" , 
+    "Modelo" : "HRV"   , 
+    "Cor"    : "Prata" 
+} 
+
+carros_json = json.dumps( carros )
+
+print( "\n\n" + str( carros_json ) + '\n' )
+
+
+
+
+
+
+
+
+
+
